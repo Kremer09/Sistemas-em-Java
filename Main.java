9eca3dafbf4d285cc4c91dac42bec9bffa58ed08
Kauã Kremer <84859962+Kremer09@users.noreply.github.com>
2022-03-18 15:11:21 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package Equação;
+
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String[] args){
+		Scanner entrada = new Scanner (System.in);
+		double a,b,c,delta,x1,x2;
+		System.out.print("Entre com o valor de A:");
+		a = entrada.nextDouble();
+		System.out.print("Entre com o valor de B:");
+		b = entrada.nextDouble();
+		System.out.print("Entre com o valor de C:");
+		c = entrada.nextDouble();
+		delta = b*b - 4*a*c;
+		if(delta>=0)
+		{	
+		  x1 = (b+Math.sqrt(delta))/(2*a);
+		  x2 = (b-Math.sqrt(delta))/(2*a);
+		  System.out.println("X1 = " + x1);
+		  System.out.println("X2 = " + x2);
+	    }
+		else System.out.println("Não é possivel calcular");
+  }
+}
